5798decf69af41aa49b9567d3f3c33d8041ed772
Sawyer Cutler <sawyer@dirtroad.dev>
2026-08-19 17:33:46 -0700

    Delete workbench-sim
    CL-6333: unused package with zero consumers in the repo.

--- packages/workbench-sim/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./scenario";
-export * from "./metrics";
-export * from "./report";
-export { busyTeamWeek } from "./scenarios/busy-team-week";
-export { TODO_SCENARIOS } from "./scenarios/todo";
